faec4c1fedfdc4772fce8b47fbbe961e4b588d02
Viktor Sukochev <sukochevvv@gmail.com>
2019-11-12 11:14:57 +0700

    Initial commit

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ISINGraph
+//
+//  Created by Victor Sukochev on 12.11.2019.
+//  Copyright © 2019 Victor Sukochev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Sources/UI/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ISINGraph
+//
+//  Created by Victor Sukochev on 12.11.2019.
+//  Copyright © 2019 Victor Sukochev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
